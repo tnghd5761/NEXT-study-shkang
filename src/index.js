5a21d325ca4cf4de3358c0059635f2344b7845cb
tnghd5761 <tnghd5761@gmail.com>
2024-10-15 01:12:26 +0900

    feat: 문서 참조하여 구현

--- src/index.js
+++ src/index.js
@@ -1,4 +1,60 @@
-const container = document.createElement('div')
+let currentStateKey = 0; // useState가 실행 된 횟수
+const states = []; // state를 보관할 배열
+function useState(initState) {
+    // initState로 초기값 설정
+    if (states.length === currentStateKey) {
+        states.push(initState);
+    }
 
-document.body.append(container)
-container.append('hello')
+    // state 할당
+    const state = states[currentStateKey];
+    const setState = (newState) => {
+        // state를 직접 수정하는 것이 아닌, states 내부의 값을 수정
+        states[currentStateKey] = newState;
+        render();
+    };
+    currentStateKey += 1;
+    return [state, setState];
+}
+
+function Counter() {
+    const [count, setCount] = useState(1);
+
+    window.increment = () => setCount(count + 1);
+
+    return `
+      <div>
+        <strong>count: ${count} </strong>
+        <button onclick="increment()">증가</button>
+      </div>
+    `;
+}
+
+function Cat() {
+    const [cat, setCat] = useState('고양이');
+
+    window.meow = () => setCat(cat + ' 야옹!');
+
+    return `
+      <div>
+        <strong>${cat}</strong>
+        <button onclick="meow()">고양이의 울음소리</button>
+      </div>
+    `;
+}
+
+const render = () => {
+    const app = document.querySelector('#app');
+
+    app.innerHTML = `
+    <div>
+      ${Counter()}
+      ${Cat()}
+    </div>
+  `;
+    // 이 시점에 currentStateKey는 2가 될 것이다.
+    // 그래서 다시 0부터 접근할 수 있도록 값을 초기화 해야 한다.
+    currentStateKey = 0;
+};
+
+render();
